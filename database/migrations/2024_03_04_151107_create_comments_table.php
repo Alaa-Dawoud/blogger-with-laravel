533b2c23a8286f07f4b37ef4af12167e3aa0a3ac
Alaa-Dawoud <48125535+Alaa-Dawoud@users.noreply.github.com>
2024-03-06 09:41:09 +0200

    Add files via upload

---
+++ database/migrations/2024_03_04_151107_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("body");
+            $table->string("name");
+            $table->string("email");
+            $table->integer("post_id");
+            $table->boolean("is_approved");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
