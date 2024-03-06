5e91e7eff83e2d368d3a6c37b658391c752af4fe
Alaa-Dawoud <48125535+Alaa-Dawoud@users.noreply.github.com>
2024-03-06 09:44:19 +0200

    Add files via upload

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Blogger</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://bootswatch.com/5/cosmo/bootstrap.min.css">
+        <link href="/css/main.css" rel="stylesheet">
+    </head>
+    <body>
+
+        <div class="container text-center" style="margin: 120px;">
+            <h1 style="margin: 80px 0px;">Welcome to blog Website</h1>
+            <h4><a href="/blog" class="btn btn-primary">Go to posts page</a></h4>
+        </div> 
+
+        <footer class="text-center">
+        <p>Copyright &copy; 2024 Blogger website</p>
+        </footer>
+        <script
+        src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+        crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    </body>
+</html>
+
