a73d95f085a72d0aec70c9454c9ca74055cb671b
Alaa-Dawoud <48125535+Alaa-Dawoud@users.noreply.github.com>
2024-03-08 18:37:50 +0200

    Add files via upload

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Category extends Model
 {
     use HasFactory;
-    protected $casts = [
-        "tags"=>"array"
-    ];
 }
