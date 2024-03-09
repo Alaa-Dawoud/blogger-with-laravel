8857d2e6c0f3a6b854edf47f228711d82edbce4c
Alaa-Dawoud <48125535+Alaa-Dawoud@users.noreply.github.com>
2024-03-09 09:41:31 +0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -26,18 +26,20 @@ Route::get('/blog/tag/{tag_name}', 'App\Http\Controllers\PostController@show_tag
 Route::get('/blog/admin/login', 'App\Http\Controllers\LoginController@login')->name("login");
 Route::post('/blog/admin/login', 'App\Http\Controllers\LoginController@authenticate');
 //admin routes
-Route::get('/blog/admin', 'App\Http\Controllers\AdminController@index')->middleware('auth');
-Route::get('/blog/admin/posts', 'App\Http\Controllers\AdminController@posts')->middleware('auth');
-Route::post('/blog/admin/posts', 'App\Http\Controllers\AdminController@store_post');
-Route::get('/blog/admin/posts/add', 'App\Http\Controllers\AdminController@create_post')->middleware('auth');
-Route::get('/blog/admin/categories', 'App\Http\Controllers\AdminController@categories')->middleware('auth');
-Route::post('/blog/admin/categories', 'App\Http\Controllers\AdminController@add_category');
-Route::get('/blog/admin/media', 'App\Http\Controllers\AdminController@media')->middleware('auth');
-Route::get('/blog/admin/comments', 'App\Http\Controllers\AdminController@comments')->middleware('auth');
-Route::post('/blog/admin/comments', 'App\Http\Controllers\AdminController@comments_action');
-Route::get('/blog/admin/tags', 'App\Http\Controllers\AdminController@tags')->middleware('auth');
-Route::get('/blog/admin/admins', 'App\Http\Controllers\AdminController@admins')->middleware('auth');
-Route::post('/blog/admin/admins', 'App\Http\Controllers\AdminController@store_admin');
+Route::group(['middleware'=>['auth']],function(){
+    Route::get('/blog/admin', 'App\Http\Controllers\AdminController@index');
+    Route::get('/blog/admin/posts', 'App\Http\Controllers\AdminController@posts');
+    Route::post('/blog/admin/posts', 'App\Http\Controllers\AdminController@store_post');
+    Route::get('/blog/admin/posts/add', 'App\Http\Controllers\AdminController@create_post');
+    Route::get('/blog/admin/categories', 'App\Http\Controllers\AdminController@categories');
+    Route::post('/blog/admin/categories', 'App\Http\Controllers\AdminController@add_category');
+    Route::get('/blog/admin/media', 'App\Http\Controllers\AdminController@media');
+    Route::get('/blog/admin/comments', 'App\Http\Controllers\AdminController@comments');
+    Route::post('/blog/admin/comments', 'App\Http\Controllers\AdminController@comments_action');
+    Route::get('/blog/admin/tags', 'App\Http\Controllers\AdminController@tags');
+    Route::get('/blog/admin/admins', 'App\Http\Controllers\AdminController@admins');
+    Route::post('/blog/admin/admins', 'App\Http\Controllers\AdminController@store_admin');    
+});
 
 
 //blog routes with route parameters
